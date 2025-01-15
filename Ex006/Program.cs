5182ae670863b3faa3e745d13e95c55e81040123
Heraldo <hccruz@gmail.com>
2025-01-15 20:21:04 -0300

    Novos Exercícios

---
+++ Ex006/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, media;
+
+            Console.Write("Digite a primeira nota: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (n1 + n2) / 2;
+
+            Console.WriteLine("Média: {0}", media);
+        }
+    }
+}
---
+++ Ex007/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final;
+            string resultado;
+
+            Console.Write("Digite a nota final do aluno: ");
+            nota_final = Convert.ToDouble(Console.ReadLine());
+
+            if (nota_final >= 70)
+            {
+                resultado = "Aprovado";
+            }
+            else if (nota_final >= 45)
+            {
+                resultado = "Recuperação";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+            Console.WriteLine("Nota do aluno: {0} - Resultado: {1}", nota_final, resultado);
+        }
+    }
+}
