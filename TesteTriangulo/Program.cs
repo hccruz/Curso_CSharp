ca62493513d9751d3327f4e75a7711e7bb041072
Heraldo <hccruz@gmail.com>
2025-01-22 21:05:04 -0300

    Novos exercícios

--- TesteTriangulo/Program.cs
+++ TesteTriangulo/Program.cs
@@ -14,6 +14,27 @@
 
             Console.Write("Digite o valor do lado C: ");
             ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("Os valores informados formam um triângulo.");
+                if ((ladoA == ladoB) && (ladoB == ladoC))
+                {
+                    Console.WriteLine("E ele é um triângulo equilátero.");
+                }
+                else if ((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC))
+                {
+                    Console.WriteLine("E ele é um triângulo isósceles.");
+                }
+                else
+                {
+                    Console.WriteLine("E ele é um triângulo escaleno.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um triângulo.");
+            }
         }
     }
 }
